b4bb955df553c1a15b79fc7bf962ee50045cae64
noahvarghese <varghese.noah@gmail.com>
2022-01-07 22:20:18 +0000

    fixed install and uninstall script

--- bin/install.sh
+++ bin/install.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-pip install -r ./requirements.txt
-
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
 fi
 
-sudo cp -R ./src /usr/local/bin/sql-runner
+pip install -r ./requirements.txt
+
+sudo cp -R ./src /usr/local/lib/sql-runner
+sudo ln -s /usr/local/lib/sql-runner/main.py /usr/local/bin/sql-runner
 
 echo "Command can now be run from the CLI as sql-runner"
---
+++ bin/uninstall.sh
@@ -0,0 +1,9 @@
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+pip uninstall -r ./requirements.txt
+
+sudo rm /usr/local/bin/sql-runner
+sudo rm -rf /usr/local/lib/sql-runner
